afe142e361f7e0032bfa8a96c6602a0ea825b2f6
Akbar-telliant <akbarkhan.p@telliant.net>
2025-08-13 20:11:44 +0530

    Adding User Configuration Screen.

---
+++ Pages/Admin/UserManagement.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using ScrumMaster.Dialog;
+using ScrumMaster.Models;
+
+namespace ScrumMaster.Pages.Admin
+{
+    public partial class UserManagement
+    {
+
+        public List<Users> users { get; set; } = new List<Users>();       
+
+        protected override async Task OnInitializedAsync()
+        {
+            users.Add( new Users("Alice", "alice@xyz", Users.TUserRole.Admin));
+            users.Add(new Users("Kumar", "kumar@xyz", Users.TUserRole.User));
+            await Task.Yield();
+        }
+
+        public async Task AddUser()
+        {
+            var parameters = new DialogParameters<UserManagementDialog>
+            {
+                { x => x.Users, new Users() }
+            };
+
+            await DialogService.ShowAsync<UserManagementDialog>(string.Empty, parameters);            
+        }
+
+        public async Task EditUser(Users value)
+        {
+            var parameters = new DialogParameters<UserManagementDialog>
+            {
+                { x => x.Users, value }
+            };
+
+            await DialogService.ShowAsync<UserManagementDialog>(string.Empty, parameters);
+        }
+
+        public async Task DeleteUser(Users value)
+        {
+            await Task.Yield();
+        }
+
+    }
+}
