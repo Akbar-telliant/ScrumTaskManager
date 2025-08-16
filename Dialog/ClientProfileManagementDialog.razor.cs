3e95a920cf0bea785a4537f345c6ca2a1ce3f1f7
Pushpalatha Ramachandran <pushpalatha.r@telliant.net>
2025-08-16 13:29:03 +0530

    Functional buttons for client profile.

---
+++ Dialog/ClientProfileManagementDialog.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using ScrumMaster.Models;
+
+namespace ScrumMaster.Dialog;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class ClientProfileManagementDialog
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CascadingParameter]
+    IMudDialogInstance? MudDialog { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public ClientProfile Client { get; set; } = new ClientProfile();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public string DialogTitle { get; set; } = "Add Client";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async Task OnValidSubmitAsync()
+    {
+        await Task.Yield();
+    }
+
+    /// <summary>
+    /// Closes the dialog without saving.
+    /// </summary>
+    private void Cancel()
+    {
+        MudDialog?.Cancel();
+    }
+}
