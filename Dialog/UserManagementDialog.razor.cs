f5186a1870493d3e1fb4268afd500bf730050baf
Pushpalatha Ramachandran <pushpalatha.r@telliant.net>
2025-08-30 20:29:38 +0530

    Tuning user management dialog

--- Dialog/UserManagementDialog.razor.cs
+++ Dialog/UserManagementDialog.razor.cs
@@ -1,21 +1,39 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using ScrumMaster.Models;
-using static ScrumMaster.Models.Users;
+using ScrumMaster.Services;
 
-namespace ScrumMaster.Dialog
+namespace ScrumMaster.Dialog;
+
+public partial class UserManagementDialog
 {
-    public partial class UserManagementDialog
+    [CascadingParameter]
+    private IMudDialogInstance? MudDialog { get; set; }
+
+    [Parameter]
+    public UserDetails User { get; set; } = new();
+
+    [Parameter]
+    public string DialogTitle { get; set; } = "Add User";
+
+    [Inject]
+    private EntityDataService<UserDetails> UserService { get; set; } = default!;
+
+    private async Task OnValidSubmitAsync()
     {
-        [CascadingParameter]
-        private IMudDialogInstance? MudDialog { get; set; }
-
-        [Parameter]
-        public Users? Users { get; set; } 
-
-        public async Task OnValidSubmitAsync()
+        if (User.UserId == 0)
         {
-            await Task.Yield();
+            // New → Add
+            await UserService.AddAsync(User);
+        }
+        else
+        {
+            // Existing → Update
+            await UserService.UpdateAsync(User);
         }
 
+        MudDialog?.Close(DialogResult.Ok(User));
     }
+
+    private void Cancel() => MudDialog?.Cancel();
 }
